783a62bbe33cd137a12b50c3560bcf6d473bf068
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2024-09-09 20:36:27 +0200

    iteration

--- src/controller.rs
+++ src/controller.rs
@@ -2,14 +2,14 @@ use std::borrow::Borrow;
 
 use axum::{extract::State, Json};
 use hyper::{header::{ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE}, StatusCode};
-use reqwest::Client;
-use fhir_sdk::r5::resources::ValueSet;
+use reqwest::{Client, Response};
+use fhir_sdk::r4b::{resources::ValueSet, types::Coding};
 
 use crate::{model::{BatchChangeRequest, BatchResponse}, Error};
 
 pub async fn process_batch(
     State(client): State<Client>,
-    Json(payload): Json<BatchChangeRequest>,
+    Json(input): Json<BatchChangeRequest>,
 ) -> (StatusCode, Json<BatchResponse>) {
     
 
@@ -17,25 +17,46 @@ pub async fn process_batch(
         "http://localhost:8080/fhir/ValueSet/$expand?url=\
         http://snomed.info/sct/45991000052106?fhir_vs=ecl/\
         {}&includeDesignations=true&count=1&filter={}",
-        payload.settings.ecl,
-        payload.settings.term
+        input.settings.ecl,
+        input.settings.term
     );
 
     println!("{}", url);
 
-    match client.get(url)
-        .header(ACCEPT_LANGUAGE, "sv")
-        .send().await {
+    match client.get(url).header(ACCEPT_LANGUAGE, "sv").send().await {
         Ok(resp) => {
-            let vs = resp.json::<ValueSet>().await.unwrap();
+            match resp.json::<ValueSet>().await {
+                Ok(vs) => {
+                    if let Some(exp) = vs.expansion.as_ref() {
+                        for contains in exp.contains.iter() {
+                            if let Some(concept) = contains.as_ref() {
+                                let sctid = concept.code.as_ref().unwrap();
 
-            let exp = &vs.expansion.as_ref().unwrap();
-
-            for concept in exp.contains.iter() {
-                println!("{concept:#?}");
-
-                
+                                let designations = concept.designation
+                                    .iter()
+                                    .filter(|&d| {
+                                        if let Some(designation) = d.as_ref() {
+                                            designation.language == Some("sv".to_string()) &&
+                                                designation.r#use.as_ref().unwrap().code.as_ref() == Some(&"900000000000013009".to_string())
+                                        } else {
+                                            false
+                                        }
+                                    
+                                    });
+                                for designation in designations {
+                                    // println!("{designation:#?}");
+                                    println!("{}: {}", sctid, designation.as_ref().unwrap().value);
+                                }
+                            }
+                        }
+                    }
+                }
+                Err(err) => {
+                    println!("{err:#?}");
+                }
             }
+
+            
             
         }
         Err(err) => {
@@ -47,7 +68,7 @@ pub async fn process_batch(
 
 
     let res = BatchResponse {
-        status: payload.settings.name,
+        status: input.settings.name,
     };
     (StatusCode::OK, Json(res))
 }
