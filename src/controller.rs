4f232056cf06b90e9953e9b940c60815fa43e11a
Daniel Karlsson <daniel.e.karlsson@gmail.com>
2024-09-18 21:59:10 +0200

    description id

--- src/controller.rs
+++ src/controller.rs
@@ -3,7 +3,7 @@ use std::borrow::Borrow;
 use axum::{extract::State, Json};
 use hyper::{header::{ACCEPT, ACCEPT_LANGUAGE, CONTENT_TYPE}, StatusCode};
 use reqwest::{Client, Response};
-use fhir_sdk::r4b::{resources::ValueSet, types::Coding};
+use fhir_sdk::r4b::{resources::ValueSet, types::{Coding, ExtensionValue}};
 
 use crate::{model::{BatchChangeRequest, BatchResponse}, Error};
 
@@ -16,7 +16,8 @@ pub async fn process_batch(
     let url = format!(
         "http://localhost:8080/fhir/ValueSet/$expand?url=\
         http://snomed.info/sct/45991000052106?fhir_vs=ecl/\
-        {}&includeDesignations=true&count=1&filter={}",
+        {}&includeDesignations=true&includeDescriptionId=true\
+        &count=1&filter={}",
         input.settings.ecl,
         input.settings.term
     );
@@ -27,29 +28,7 @@ pub async fn process_batch(
         Ok(resp) => {
             match resp.json::<ValueSet>().await {
                 Ok(vs) => {
-                    if let Some(expansion) = vs.expansion.as_ref() {
-                        for contains in expansion.contains.iter() {
-                            if let Some(concept) = contains.as_ref() {
-                                let code = concept.code.as_ref().unwrap();
-
-                                let designations = concept.designation
-                                    .iter()
-                                    .filter(|&d| {
-                                        if let Some(designation) = d.as_ref() {
-                                            designation.language == Some("sv".to_string()) &&
-                                                designation.r#use.as_ref().unwrap().code.as_ref() == Some(&"900000000000013009".to_string())
-                                        } else {
-                                            false
-                                        }
-                                    
-                                    });
-                                for designation in designations {
-                                    // println!("{designation:#?}");
-                                    println!("{}: {}", code, designation.as_ref().unwrap().value);
-                                }
-                            }
-                        }
-                    }
+                    process_valueset(vs);
                 }
                 Err(err) => {
                     println!("{err:#?}");
@@ -71,4 +50,40 @@ pub async fn process_batch(
         status: input.settings.name,
     };
     (StatusCode::OK, Json(res))
+}
+
+fn process_valueset(vs: ValueSet) -> Option<()>{
+    if let Some(expansion) = vs.expansion.as_ref() {
+        for contains in expansion.contains.iter() {
+            if let Some(concept) = contains.as_ref() {
+                let code = concept.code.as_ref()?;
+
+                let designations = concept.designation
+                    .iter()
+                    .filter(|&d| {
+                        if let Some(designation) = d.as_ref() {
+                            if let Some(r#use) = designation.r#use.as_ref() {
+                                designation.language == Some("sv".to_string()) &&
+                                    r#use.code.as_ref() == Some(&"900000000000013009".to_string())
+                            } else {
+                                false
+                            }
+                        } else {
+                            false
+                        }
+                    
+                    });
+                for designation in designations {
+                    // println!("{designation:#?}");
+                    let value = designation.as_ref()?.extension.iter().find(|&e| {
+                        e.url.eq("http://hl7.org/fhir/StructureDefinition/coding-sctdescid") 
+                    })?.value.as_ref()?;
+                    if let ExtensionValue::String(desc_id) = &value {
+                        println!("{}: {} ({})", code, designation.as_ref()?.value, desc_id);
+                    }                   
+                }
+            }
+        }
+    }
+    Some(())
 }
